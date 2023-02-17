09bbf6347cc87bf86932559684417f62e494efa1
Nolan <iamchunkerdunkers@gmail.com>
2023-02-17 10:09:24 -0700

    notes

---
+++ notes/NotesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotesViewController.swift
+//  notes
+//
+//  Created by Laake, Nolan - Student on 2/15/23.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+
+    public var noteTitle: String = ""
+    public var note: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+
+
+}
